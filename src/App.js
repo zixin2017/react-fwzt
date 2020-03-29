29741161350aa160631c898446d26c272aee295d
2 <2>
2020-03-30 05:27:54 +0800

    slc

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Person from './Pages/Person/person'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <person />
+            </header>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Pages/Person/person.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const person = () => {
+    return 'HEllO'
+};
+
+export default person;
